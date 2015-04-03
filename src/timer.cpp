3954b2d7bd12cdb04c5233b52c0d2f3382681dd9
claytonrogers <claytonrogers53@gmail.com>
2015-04-03 17:41:38 -0400

    Completely rewrote the timer class because it was terrible.

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,47 +1,38 @@
+#include <stdexcept>
 #include "timer.h"
 
-double CTimer::readTimer() {
-    // use the <sys/time.h> to get time in s and us convert to ms
-    double elapsedTime;
+CTimer::CTimer() :
+        is_started(false),
+        is_elapsed_time_set(false)
+{}
 
-    elapsedTime = (stopTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
-    elapsedTime += (stopTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
-
-    return elapsedTime;
+void CTimer::startTimer() {
+    // Reset start and stop time to now
+    // The start time is last so that getting the stop time is not included in the run
+    gettimeofday(&startTime, NULL);
+    is_started = true;
+    is_elapsed_time_set = false;
 }
 
-double CTimer::readTimerSec() {
-    // use the <sys/time.h> to get time in s and us convert to ms, then return in s
-    double elapsedTime;
+void CTimer::stopTimer() {
+    if (!is_started) {
+        throw new std::logic_error("Tried to stop a timer that was not started.");
+    }
+    // Set the stop time to now
+    gettimeofday(&stopTime, NULL);
+    is_started = false;
 
-    elapsedTime = (stopTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
-    elapsedTime += (stopTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
-
-    return elapsedTime/1000.0;
+    elapsed_time  = (stopTime.tv_sec  - startTime.tv_sec) * 1000.0;
+    elapsed_time += (stopTime.tv_usec - startTime.tv_usec) / 1000.0;
+    is_elapsed_time_set = true;
 }
 
-double CTimer::readLapTime() {
-    // Create a new stoptime without affecting any private variable
-    timeval lapTime;
-    gettimeofday(&lapTime, NULL);
 
-    double elapsedTime;
-
-    elapsedTime = (lapTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
-    elapsedTime += (lapTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
-
-    return elapsedTime;
+double CTimer::readTimer() const{
+    if (is_elapsed_time_set) {
+        return elapsed_time;
+    } else {
+        throw new std::logic_error("Tried to read a timer that has not been stopped.");
+    }
 }
 
-double CTimer::readLapTimeSec() {
-    // Create a new stoptime without affecting any private variable
-    timeval lapTime;
-    gettimeofday(&lapTime, NULL);
-
-    double elapsedTime;
-
-    elapsedTime = (lapTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
-    elapsedTime += (lapTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
-
-    return elapsedTime/1000.0;
-}
--- src/timer.h
+++ src/timer.h
@@ -1,83 +1,36 @@
-/*	timer.h		v0.3
- * 
- * class CTimer
- * 		Creates a timer that can be started stopped
- * 		and has a laptimer. Precision good to at least
- * 		0.01 milliseconds
- * 
- * 
- * DEPENDENCIES:
- * 	<sys/time.h>
- * 		
- * 
- * TODO:
- * 	make a way of restarting from the previous time value
- * 	^ this can be done fairly easily from within the program
- * 
- * 
- * 
- * FUNCTIONS:
- * 
- * startTime
- * 	reset and starts the clock
- * 	note that when the timer is created it starts running
- * 
- * stopTime
- * 	stops the clock
- * 	note the time can only be read once the timer has been stopped
- * 
- * readTimer
- * 	get the time that was on the clock when it was stopped
- * 	outputs in milliseconds
- * 
- * readTimerSec
- * 	get the time that was on the clock when it was stopped
- * 	outputs in seconds
- * 
- * readLapTime
- * 	returns the time that is currently on the timer without
- * 	stopping it
- * 	outputs in milliseconds
- * 
- * readLapTimeSec
- * 	returns the time that is currently on the timer without
- * 	stopping it
- * 	outputs in seconds
- * 
- * Clayton Rogers
- * Mar 2010
- */
-
-
 #ifndef TIMER_H
 #define TIMER_H
 
-#include <sys/time.h>			// for gettimeofday()
+#include <sys/time.h>
 
+/**
+ * Creates a timer that can be started stopped. Precision good to at least 0.01
+ * milliseconds.
+ */
 class CTimer {
     timeval startTime;
     timeval stopTime;
+    bool is_started;
+    bool is_elapsed_time_set;
+    double elapsed_time; // in ms
 public:
-    CTimer () {
-        // Set both start and stop time to now
-        // The start time is last so that getting the stoptime is not included in the run
-        gettimeofday(&stopTime, NULL);
-        gettimeofday(&startTime, NULL);
-    }
-    void startTimer () {
-        // Reset start and stop time to now
-        // The start time is last so that getting the stoptime is not included in the run
-        gettimeofday(&stopTime, NULL);
-        gettimeofday(&startTime, NULL);
-    }
-    void stopTimer () {
-        // Set the stop time to now
-        gettimeofday(&stopTime, NULL);
-    }
-    double readTimer();
-    double readTimerSec();
-    double readLapTime();
-    double readLapTimeSec();
+    CTimer ();
+    /**
+     * Starts the timer.
+     */
+    void startTimer ();
+
+    /**
+     * Stops the timer. It must be started before it can be stopped.
+     */
+    void stopTimer ();
+
+    /**
+     * Reads the value on the timer. The timer must be stopped before it can be read.
+     *
+     * Returns: The time (in ms) on the timer when it was stopped.
+     */
+    double readTimer() const;
 };
 
 #endif
