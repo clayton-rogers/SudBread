2a480412728cd1f71d24a552b9f6080794e986f8
claytonrogers <claytonrogers53@gmail.com>
2015-04-04 12:25:22 -0400

    Removed declaration that is never used.

--- src/timetrial.h
+++ src/timetrial.h
@@ -4,6 +4,5 @@
 void RunUnitTest (int MAX);				// Run the unit test and output the results
                     // Asks the user what base filename and then creates the rest
 void GetFilename (char basename[]);
-void GetNextFilename (char basename[], int num, char filename[]);	// Loads the required filename
 
 #endif
